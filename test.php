03ae6cfe54fce5aea5774dd7b3794b57741ed8f7
baiker <57838797+Baikeryoyo@users.noreply.github.com>
2022-03-21 14:27:06 +0800

    Create test.php

---
+++ test.php
@@ -0,0 +1,52 @@
+<?php
+function real_ip()
+{
+    $ip = $_SERVER['REMOTE_ADDR'];
+    if (isset($_SERVER['HTTP_X_FORWARDED_FOR']) && preg_match_all('#\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}#s', $_SERVER['HTTP_X_FORWARDED_FOR'], $matches)) {
+        foreach ($matches[0] AS $xip) {
+            if (!preg_match('#^(10|172\.16|192\.168)\.#', $xip)) {
+                $ip = $xip;
+                break;
+            }
+        }
+    } elseif (isset($_SERVER['HTTP_CLIENT_IP']) && preg_match('/^([0-9]{1,3}\.){3}[0-9]{1,3}$/', $_SERVER['HTTP_CLIENT_IP'])) {
+        $ip = $_SERVER['HTTP_CLIENT_IP'];
+    } elseif (isset($_SERVER['HTTP_CF_CONNECTING_IP']) && preg_match('/^([0-9]{1,3}\.){3}[0-9]{1,3}$/', $_SERVER['HTTP_CF_CONNECTING_IP'])) {
+        $ip = $_SERVER['HTTP_CF_CONNECTING_IP'];
+    } elseif (isset($_SERVER['HTTP_X_REAL_IP']) && preg_match('/^([0-9]{1,3}\.){3}[0-9]{1,3}$/', $_SERVER['HTTP_X_REAL_IP'])) {
+        $ip = $_SERVER['HTTP_X_REAL_IP'];
+    }
+    return $ip;
+
+}
+echo real_ip();
+echo "<br>"."++++++++++++++++++++++++++++++++++++"."<br>";
+function getIp() {
+    foreach (array(
+                 'HTTP_CLIENT_IP',
+                 'HTTP_X_FORWARDED_FOR',
+                 'HTTP_X_FORWARDED',
+                 'HTTP_X_CLUSTER_CLIENT_IP',
+                 'HTTP_FORWARDED_FOR',
+                 'HTTP_FORWARDED',
+                 'REMOTE_ADDR') as $key) {
+        if (array_key_exists($key, $_SERVER)) {
+            foreach (explode(',', $_SERVER[$key]) as $ip) {
+                $ip = trim($ip);
+                //会过滤掉保留地址和私有地址段的IP，例如 127.0.0.1会被过滤
+                //也可以修改成正则验证IP
+                if ((bool) filter_var($ip, FILTER_VALIDATE_IP,
+                    FILTER_FLAG_IPV4 |
+                    FILTER_FLAG_NO_PRIV_RANGE |
+                    FILTER_FLAG_NO_RES_RANGE)) {
+                    return $ip;
+                }
+            }
+        }
+    }
+    return null;
+}
+
+$real_IP = getIp();
+var_dump($real_IP);
+?>
